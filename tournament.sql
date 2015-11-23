9f2be607fe8571552a391ef0b2d23053970aefd8
C. M <greencopperroof@gmail.com>
2015-11-23 00:35:33 +0000

    removed unnecessary sql and updated README

--- tournament.sql
+++ tournament.sql
@@ -4,11 +4,6 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-SELECT pg_terminate_backend(pg_stat_activity.pid)
-FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'tournament'
-  AND pid <> pg_backend_pid();
-
 DROP DATABASE IF EXISTS tournament; 
 
 CREATE DATABASE tournament;
